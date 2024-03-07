2be8c60443da5455e7650616ca27cfd6799f8930
nikola.stanojkovic <nikolastanojkovic3@gmail.com>
2024-03-07 17:28:42 +0100

    Solve Day8_Part1(WIP)

---
+++ Day8_Part1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+
+Documents ReadDocumentsFromInput(string filePath)
+{
+	using var reader = new StreamReader(filePath);
+
+	var instructions = new List<char>();
+
+	var currentChar = (char)reader.Read();
+
+	while (currentChar is not '\n')
+	{
+		instructions.Add(currentChar);
+		currentChar = (char)reader.Read();
+	}
+
+	reader.ReadLine(); //empty row 
+
+	
+}
+
+record Node
+{
+	public string Destination { get; init; } = string.Empty;
+	public string LeftPath { get; init; } = string.Empty;
+	public string RightPath { get; init; } = string.Empty;
+
+	public Node(string line)
+	{
+		//^(\w+) = \((\w+), (\w+)\)$ <- regex for reading nodes
+	}
+}
+
+class Documents
+{
+	char[] Instructions { get; init; } = [];
+	SortedDictionary<string, Node> Network { get; init; } = [];
+}
+
