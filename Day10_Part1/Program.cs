d26bdc66d958f3f42b8a1579a0af178bfa140423
nikola.stanojkovic <nikolastanojkovic3@gmail.com>
2024-07-01 23:21:27 +0200

    Solve Day9_Part2, Day10_Part1 WIP

---
+++ Day10_Part1/Program.cs
@@ -0,0 +1,70 @@
+﻿void Solve()
+{
+	var grid = ReadGridFromInput("test.txt");
+}
+
+Solve();
+
+
+//TODO Convert return value to 2d array for easier traversal
+List<List<Tile>> ReadGridFromInput(string filePath)
+{
+	using var reader = new StreamReader(filePath);
+	var grid = new List<List<Tile>>();
+
+	while(!reader.EndOfStream)
+	{
+		var line = reader.ReadLine();
+		var tileLine = new List<Tile>();
+
+		if (line is null)
+			break;
+
+		foreach(var tileChar in line)
+		{
+			tileLine.Add(ParseTile(tileChar));
+		}
+
+		grid.Add(tileLine);
+	}
+
+	return grid;
+}
+
+Tile ParseTile(char tileChar)
+{
+	return tileChar switch
+	{
+		'|' => new Tile(TileType.Pipe, Cardinal.North, Cardinal.South),
+		'-' => new Tile(TileType.Pipe, Cardinal.East, Cardinal.West),
+		'L' => new Tile(TileType.Pipe, Cardinal.North, Cardinal.East),
+		'J' => new Tile(TileType.Pipe, Cardinal.North, Cardinal.West),
+		'7' => new Tile(TileType.Pipe, Cardinal.South, Cardinal.West),
+		'F' => new Tile(TileType.Pipe, Cardinal.South, Cardinal.East),
+		'.' => new Tile(TileType.Ground),
+		'S' => new Tile(TileType.Start),
+		_ => throw new ArgumentException("Invalid character.")
+	};
+}
+
+struct Tile(TileType type, Cardinal? from = null, Cardinal? to = null)
+{
+	public TileType Type = type;
+	public Cardinal? From = from;
+	public Cardinal? To = to;
+}
+
+enum TileType
+{
+	Pipe,
+	Ground,
+	Start
+}
+
+enum Cardinal
+{
+	North,
+	South,
+	East,
+	West
+}
---
+++ Day9_Part2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics;
+
+void Solve()
+{
+	var sw = Stopwatch.StartNew();
+
+	var histories = ReadHistoriesFromInput("input.txt");
+	var result = 0;
+	foreach (var history in histories)
+	{
+		result += CalculateNextValue(history);
+	}
+
+	sw.Stop();
+	Console.WriteLine($"Result: {result}, Time: {sw.ElapsedMilliseconds}ms");
+}
+
+Solve();
+
+static IEnumerable<List<int>> ReadHistoriesFromInput(string filePath)
+{
+	using var reader = new StreamReader(filePath);
+
+	while (!reader.EndOfStream)
+	{
+		var newHistory = reader.ReadLine()!
+			.Split(' ')
+			.Select(int.Parse)
+			.ToList();
+
+		yield return newHistory;
+	}
+}
+
+int CalculateNextValue(List<int> history)
+{
+	var currentSequence = history;
+	var allSequences = new List<List<int>>();
+
+	while (currentSequence.Any(num => num != 0))
+	{
+		allSequences.Add(currentSequence);
+		var newSequence = new List<int>();
+
+		for (int i = 1; i < currentSequence.Count; i++)
+		{
+			newSequence.Add(currentSequence[i] - currentSequence[i - 1]);
+		}
+
+		currentSequence = newSequence;
+	}
+
+	allSequences.Reverse();
+	var result = 0;
+
+	foreach (var sequence in allSequences)
+	{
+		result = sequence.First() - result;
+	}
+
+	return result;
+}
