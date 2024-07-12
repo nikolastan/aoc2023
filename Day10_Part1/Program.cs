dd659ec51e6bbc6f1236963c1b7c98f2d03be2b2
Nikola Stanojkovic <nikola.stanojkovic@naissus.tech>
2024-07-12 16:17:42 +0200

    Solve for test case, input still WIP

--- Day10_Part1/Program.cs
+++ Day10_Part1/Program.cs
@@ -1,12 +1,19 @@
-﻿void Solve()
+﻿using System.Diagnostics;
+
+void Solve()
 {
+    var sw = Stopwatch.StartNew();
+
     var grid = ReadGridFromInput("test.txt");
 
     var (i, j) = FindStartIndex(grid);
 
     ScanSurroundings(ref grid, i, j, 0);
 
-    var message = string.Empty;
+    var result = (grid.Max(x => x.Max(x => x.DistanceFromStart)) + 1) / 2;
+
+    sw.Stop();
+    Console.WriteLine($"Result: {result}, Time: {sw.ElapsedMilliseconds}ms");
 }
 
 Solve();
@@ -49,37 +56,33 @@ Tile[][] ReadGridFromInput(string filePath)
     throw new InvalidOperationException("There is no Start tile in input. Check your input.");
 }
 
-void ScanSurroundings(ref Tile[][] grid, int i, int j, int currentDistanceFromStart, Cardinal? direction = null)
+void ScanSurroundings(ref Tile[][] grid, int i, int j, int currentDistanceFromStart)
 {
     if (i == grid.Length || i < 0
         || j == grid[i].Length || j < 0)
         return;
 
-    if (grid[i - 1][j].From == Cardinal.South || grid[i - 1][j].To == Cardinal.South)
-        ScanSurroundings(ref grid, i - 1, j, currentDistanceFromStart, Cardinal.North);
-
-    if (grid[i][j - 1].From == Cardinal.East || grid[i][j - 1].To == Cardinal.East)
-        ScanSurroundings(ref grid, i, j - 1, currentDistanceFromStart, Cardinal.West);
-
-    if (grid[i + 1][j].From == Cardinal.North || grid[i + 1][j].To == Cardinal.North)
-        ScanSurroundings(ref grid, i + 1, j, currentDistanceFromStart, Cardinal.South);
-
-    if (grid[i][j + 1].From == Cardinal.West || grid[i][j + 1].To == Cardinal.West)
-        ScanSurroundings(ref grid, i, j + 1, currentDistanceFromStart, Cardinal.East);
-
-    if (direction is null)
+    if (grid[i][j].Visited)
         return;
 
-    var currentTile = grid[i][j];
+    grid[i][j].Visited = true;
 
-    if (currentTile.Type != TileType.Start && currentTile.DistanceFromStart > currentDistanceFromStart)
+    if (grid[i][j].Type is not TileType.Start && (grid[i][j].DistanceFromStart is null || grid[i][j].DistanceFromStart < currentDistanceFromStart))
     {
-        currentTile.DistanceFromStart = ++currentDistanceFromStart;
+        grid[i][j].DistanceFromStart = ++currentDistanceFromStart;
     }
-    else
-    {
-        return;
-    }
+
+    if (i > 0 && (grid[i - 1][j].From == Cardinal.South || grid[i - 1][j].To == Cardinal.South))
+        ScanSurroundings(ref grid, i - 1, j, currentDistanceFromStart);
+
+    if (j > 0 && (grid[i][j - 1].From == Cardinal.East || grid[i][j - 1].To == Cardinal.East))
+        ScanSurroundings(ref grid, i, j - 1, currentDistanceFromStart);
+
+    if (i < grid.Length - 1 && (grid[i + 1][j].From == Cardinal.North || grid[i + 1][j].To == Cardinal.North))
+        ScanSurroundings(ref grid, i + 1, j, currentDistanceFromStart);
+
+    if (j < grid.Length - 1 && (grid[i][j + 1].From == Cardinal.West || grid[i][j + 1].To == Cardinal.West))
+        ScanSurroundings(ref grid, i, j + 1, currentDistanceFromStart);
 }
 
 Tile ParseTile(char tileChar)
@@ -103,7 +106,8 @@ struct Tile(TileType type, Cardinal? from = null, Cardinal? to = null)
     public TileType Type = type;
     public Cardinal? From = from;
     public Cardinal? To = to;
-    public int DistanceFromStart = 0;
+    public int? DistanceFromStart = null;
+    public bool Visited = false;
 }
 
 enum TileType
