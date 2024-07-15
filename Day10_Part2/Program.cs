7ceef208013bad4efab1794523cc68bd4b40895a
nikola.stanojkovic <nikolastanojkovic3@gmail.com>
2024-07-15 22:24:07 +0200

    Day10_Part2 WIP (we must check for inner border of loop)

--- Day10_Part2/Program.cs
+++ Day10_Part2/Program.cs
@@ -10,7 +10,9 @@ void Solve()
 
     ScanSurroundings(ref grid, i, j, 0);
 
-    var result = (grid.Max(x => x.Max(x => x.DistanceFromStart)) + 1) / 2;
+
+
+    var result = 0;
 
     sw.Stop();
     Console.WriteLine($"Result: {result}, Time: {sw.ElapsedMilliseconds}ms");
@@ -56,16 +58,14 @@ Tile[][] ReadGridFromInput(string filePath)
     throw new InvalidOperationException("There is no Start tile in input. Check your input.");
 }
 
-void ScanSurroundings(ref Tile[][] grid, int i, int j, int currentDistanceFromStart)
+void ScanSurroundings(ref Tile[][] grid, int i, int j)
 {
     bool changed;
-    Cardinal currentLoopDirection;
-
     do
     {
         changed = false;
 
-        grid[i][j].Visited = true;
+        grid[i][j].PartOfLoop = true;
 
         if (grid[i][j].EligibleForEnclosure)
         {
@@ -74,48 +74,79 @@ void ScanSurroundings(ref Tile[][] grid, int i, int j, int currentDistanceFromSt
 
         if (i > 0
             && ArePipesConnected(grid[i - 1][j], grid[i][j], Cardinal.South, Cardinal.North)
-            && !grid[i - 1][j].Visited)
+            && !grid[i - 1][j].PartOfLoop)
         {
             i--;
             changed = true;
-            currentLoopDirection = Cardinal.North;
+            MarkAdjacentForEnclosure(ref grid, i, j);
         }
         else if (j > 0
             && ArePipesConnected(grid[i][j - 1], grid[i][j], Cardinal.East, Cardinal.West)
-            && !grid[i][j - 1].Visited)
+            && !grid[i][j - 1].PartOfLoop)
         {
             j--;
             changed = true;
-            currentLoopDirection = Cardinal.West;
         }
         else if (i < grid.Length - 1
             && ArePipesConnected(grid[i + 1][j], grid[i][j], Cardinal.North, Cardinal.South)
-            && !grid[i + 1][j].Visited)
+            && !grid[i + 1][j].PartOfLoop)
         {
             i++;
             changed = true;
-            currentLoopDirection = Cardinal.South;
-        }
+			MarkAdjacentForEnclosure(ref grid, i, j);
+		}
         else if (j < grid.Length - 1
             && ArePipesConnected(grid[i][j + 1], grid[i][j], Cardinal.West, Cardinal.East)
-            && !grid[i][j + 1].Visited)
+            && !grid[i][j + 1].PartOfLoop)
         {
             j++;
             changed = true;
-            currentLoopDirection = Cardinal.East;
         }
     }
     while (changed);
 }
 
-void MarkForEnclosure(ref Tile[][] grid, int i, int j, Cardinal currentLoopDirection)
+void MarkAdjacentForEnclosure(ref Tile[][] grid, int i, int j)
 {
+    if(j < grid.Length - 1 && !grid[i][j + 1].PartOfLoop)
+    {
+        grid[i][j + 1].EligibleForEnclosure = true;
+    }
 
+    if(j > 0 && !grid[i][j - 1].PartOfLoop)
+    {
+        grid[i][j - 1].EligibleForEnclosure = true;
+    }
 }
 
 bool ArePipesConnected(Tile tile1, Tile tile2, Cardinal from, Cardinal to)
 {
     return (tile1.From == from || tile1.To == from) && (tile2.From == to || tile2.To == to || tile2.Type == TileType.Start);
+}
+
+int CountEnclosedTiles(ref Tile[][] grid)
+{
+    var eligibleForEnclosure = grid
+        .SelectMany((row, rowIndex) => row.Select((_, colIndex) => new { rowIndex, colIndex }))
+        .ToList();
+
+	var totalCount = 0;
+
+    while(eligibleForEnclosure.Count > 0)
+    {
+        var currentTile = eligibleForEnclosure.First();
+        eligibleForEnclosure.Remove(currentTile);
+
+        var rowCount = 1;
+
+        //Cannot work like this, we must check for inner & outer border of loop.
+    }
+
+}
+
+bool IsEnclosed(ref Tile[][] grid, int i, int j)
+{
+
 }
 
 Tile ParseTile(char tileChar)
@@ -139,7 +170,7 @@ struct Tile(TileType type, Cardinal? from = null, Cardinal? to = null)
     public TileType Type = type;
     public Cardinal? From = from;
     public Cardinal? To = to;
-    public bool Visited = false;
+    public bool PartOfLoop = false;
     public bool EligibleForEnclosure = false;
 }
 
