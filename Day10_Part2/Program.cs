be2859d2d7a5e7a5cd04e8dd96b57abd778e6491
Nikola Stanojkovic <nikola.stanojkovic@naissus.tech>
2024-07-22 12:43:02 +0200

    Day10_Part2 still WIP... I want to redo everything

--- Day10_Part2/Program.cs
+++ Day10_Part2/Program.cs
@@ -118,6 +118,7 @@ bool IsInsideLoop(ref Tile[][] grid, int i, int j)
     var totalEdgeCount = 0;
     var prevColIndex = -1;
     var isCurrentlyOnEdge = false;
+    var currentEdgeLenght = 0;
 
     while (j < grid[0].Length)
     {
@@ -126,25 +127,34 @@ bool IsInsideLoop(ref Tile[][] grid, int i, int j)
         {
             if (!isCurrentlyOnEdge)
             {
+                currentEdgeLenght = 0;
                 totalEdgeCount++;
                 isCurrentlyOnEdge = true;
+                prevColIndex = j;
+            }
+            else
+            {
+                currentEdgeLenght++;
             }
 
-            prevColIndex = j;
-
-
-            if (j < grid[0].Length - 1 && !ArePipesConnected(currentTile, grid[i][j + 1]))
+            if (((j < grid[0].Length - 1 && !ArePipesConnected(currentTile, grid[i][j + 1], Cardinal.East)) || j == grid[0].Length - 1)
+                && prevColIndex != -1
+                && currentEdgeLenght > 0)
             {
                 if (i > 0
-                && ArePipesConnected(grid[i][prevColIndex], grid[i - 1][prevColIndex])
-                && grid[i - 1][prevColIndex].PartOfLoop)
+                && ArePipesConnected(grid[i][prevColIndex], grid[i - 1][prevColIndex], Cardinal.North)
+                && grid[i - 1][prevColIndex].PartOfLoop
+                && ArePipesConnected(grid[i][j], grid[i - 1][j], Cardinal.North)
+                && grid[i - 1][j].PartOfLoop)
                 {
                     totalEdgeCount++;
                     isCurrentlyOnEdge = false;
                 }
                 else if (i < grid.Length - 1
-                    && ArePipesConnected(grid[i][prevColIndex], grid[i + 1][prevColIndex])
-                    && grid[i + 1][prevColIndex].PartOfLoop)
+                    && ArePipesConnected(grid[i][prevColIndex], grid[i + 1][prevColIndex], Cardinal.South)
+                    && grid[i + 1][prevColIndex].PartOfLoop
+                    && ArePipesConnected(grid[i][j], grid[i + 1][j], Cardinal.South)
+                    && grid[i + 1][j].PartOfLoop)
                 {
                     totalEdgeCount++;
                     isCurrentlyOnEdge = false;
@@ -168,13 +178,20 @@ bool IsInsideLoop(ref Tile[][] grid, int i, int j)
 
 bool ArePipesConnectedBySides(Tile tile1, Tile tile2, Cardinal from, Cardinal to)
 {
-    return (tile1.From == from || tile1.To == from) && (tile2.From == to || tile2.To == to || tile2.Type == TileType.Start);
+    return (tile1.From == from || tile1.To == from || tile1.Type == TileType.Start)
+        && (tile2.From == to || tile2.To == to || tile2.Type == TileType.Start);
 }
 
-bool ArePipesConnected(Tile tile1, Tile tile2)
+bool ArePipesConnected(Tile tile1, Tile tile2, Cardinal goingTo)
 {
-    return ArePipesConnectedBySides(tile1, tile2, Cardinal.North, Cardinal.South)
-        || ArePipesConnectedBySides(tile1, tile2, Cardinal.East, Cardinal.West);
+    return goingTo switch
+    {
+        Cardinal.East => ArePipesConnectedBySides(tile1, tile2, Cardinal.East, Cardinal.West),
+        Cardinal.West => ArePipesConnectedBySides(tile1, tile2, Cardinal.West, Cardinal.East),
+        Cardinal.North => ArePipesConnectedBySides(tile1, tile2, Cardinal.North, Cardinal.South),
+        Cardinal.South => ArePipesConnectedBySides(tile1, tile2, Cardinal.South, Cardinal.North),
+        _ => false,
+    };
 }
 
 Tile ParseTile(char tileChar)
