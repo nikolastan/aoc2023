04bac15ff180aa0fd06f4ed233d536bfcf945a3f
Nikola Stanojkovic <nikola.stanojkovic@naissus.tech>
2024-07-26 11:33:19 +0200

    Day12 WIP

---
+++ Day12/Program.cs
@@ -0,0 +1,19 @@
+﻿void SolvePart1()
+{
+
+}
+
+SolvePart1();
+
+IEnumerable<string> ReadRecords(string filePath)
+{
+    using var reader = new StreamReader(filePath);
+
+    while (!reader.EndOfStream)
+    {
+        var line = reader.ReadLine();
+
+        if (line is not null)
+            yield return line.Trim('.');
+    }
+}
