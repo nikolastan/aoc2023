ea184257814b6d06900afdb79a1c7a3572792249
Nikola Stanojkovic <nikola.stanojkovic@naissus.tech>
2024-08-01 16:34:17 +0200

    Day12 WIP (bugged solution)

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -1,6 +1,24 @@
-﻿void SolvePart1()
+﻿using System.Diagnostics;
+
+void SolvePart1()
 {
+    var sw = Stopwatch.StartNew();
+
     var records = ReadRecords("test.txt");
+
+    var result = 0;
+
+    foreach (var item in records)
+    {
+        result += CalculateTotalArrangements(item.Conditions, item.GroupSizes);
+    }
+
+    //var result = records.Select(x => CalculateTotalArrangements(x.Conditions, x.GroupSizes))
+    //    .Sum();
+
+    sw.Stop();
+
+    Console.WriteLine($"Result: {result}, Time elapsed: {sw.ElapsedMilliseconds}ms.");
 }
 
 SolvePart1();
@@ -24,6 +42,37 @@ IEnumerable<Record> ReadRecords(string filePath)
                 .ToList());
         }
     }
+}
+
+int CalculateTotalArrangements(string conditions, List<int> groupSizes)
+{
+    if (groupSizes.Count is 0)
+        return 1;
+
+    if (conditions.All(x => x is not '.') && conditions.Length == groupSizes.First())
+        return 1;
+
+    if (conditions.Count(x => x is not '.') < groupSizes.Sum())
+        return 0;
+
+    var totalArrangements = 0;
+
+    if (conditions[..groupSizes.First()].All(x => x is not '.')
+        && conditions.Length > groupSizes.First()
+        && conditions[groupSizes.First()] is not '#')
+    {
+        totalArrangements += CalculateTotalArrangements(conditions[(groupSizes.First() + 1)..], groupSizes[1..]);
+        for (int i = 1; i <= groupSizes.First(); i++)
+        {
+            totalArrangements += CalculateTotalArrangements(conditions[i..], groupSizes);
+        }
+    }
+    else
+    {
+        totalArrangements += CalculateTotalArrangements(conditions[1..], groupSizes);
+    }
+
+    return totalArrangements;
 }
 
 struct Record(string conditions, List<int> groupSizes)
