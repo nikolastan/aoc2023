0cdeeabbc8d9961e88d2b524dc14fb72e0362ccf
Nikola Stanojkovic <nikola.stanojkovic@naissus.tech>
2024-08-02 12:57:23 +0200

    Solve Day12 both part1 & part2

--- Day12/Solution.cs
+++ Day12/Solution.cs
@@ -13,25 +13,54 @@ public class Solution
     }
 
     [Test]
+    public void Part1_Test1()
+    {
+        var result = SolvePart1("test1.txt");
+        Assert.That(result, Is.EqualTo(2));
+    }
+
+    [Test]
+    public void Part2_Example()
+    {
+        var result = SolvePart2("test.txt");
+        Assert.That(result, Is.EqualTo(525152));
+    }
+
+    [Test]
     public void Part1_Input()
     {
         SolvePart1("input.txt");
     }
 
-    int SolvePart1(string inputPath)
+    [Test]
+    public void Part2_Input()
+    {
+        SolvePart2("input.txt");
+    }
+
+    long SolvePart1(string inputPath)
     {
         var sw = Stopwatch.StartNew();
 
         var records = ReadRecords(inputPath);
 
-        //var result = 0;
+        var result = records.Select(x => CalculateArrangements(x.Conditions, x.GroupSizes, []))
+            .Sum();
 
-        //foreach (var item in records)
-        //{
-        //    result += CalculateTotalArrangements(item.Conditions, item.GroupSizes);
-        //}
+        sw.Stop();
 
-        var result = records.Select(x => CalculateArrangements(x.Conditions, x.GroupSizes))
+        Console.WriteLine($"Result: {result}, Time elapsed: {sw.ElapsedMilliseconds}ms.");
+
+        return result;
+    }
+
+    long SolvePart2(string inputPath)
+    {
+        var sw = Stopwatch.StartNew();
+
+        var records = ReadUnfoldedRecords(inputPath);
+
+        var result = records.Select(x => CalculateArrangements(x.Conditions, x.GroupSizes, []))
             .Sum();
 
         sw.Stop();
@@ -62,10 +91,36 @@ public class Solution
         }
     }
 
-    int CalculateArrangements(string conditions, List<int> groupSizes)
+    IEnumerable<Record> ReadUnfoldedRecords(string filePath)
+    {
+        using var reader = new StreamReader(filePath);
+
+        while (!reader.EndOfStream)
+        {
+            var line = reader.ReadLine();
+
+            if (line is not null)
+            {
+                var inputs = line.Split(' ');
+
+                yield return new Record(string.Join('?', Enumerable.Repeat($"{inputs[0]}", 5)),
+                    string.Join(',', Enumerable.Repeat($"{inputs[1]}", 5))
+                    .Split(',')
+                    .Select(int.Parse)
+                    .ToList());
+            }
+        }
+    }
+
+    long CalculateArrangements(string conditions, List<int> groupSizes, Dictionary<string, long> memo)
     {
         if (groupSizes.Count is 0)
-            return 1;
+        {
+            if (conditions.Contains('#'))
+                return 0;
+            else
+                return 1;
+        }
 
         if (conditions.All(x => x is not '.')
             && conditions.Length == groupSizes.First()
@@ -75,19 +130,29 @@ public class Solution
         if (conditions.Count(x => x is not '.') < groupSizes.Sum())
             return 0;
 
-        var totalArrangements = 0;
+        var totalArrangements = 0L;
+
+        if (memo.TryGetValue(GetMemoKey(conditions, groupSizes), out totalArrangements))
+            return totalArrangements;
 
         if (conditions[..groupSizes.First()].All(x => x is not '.')
             && conditions.Length > groupSizes.First()
             && conditions[groupSizes.First()] is not '#')
         {
-            totalArrangements += CalculateArrangements(conditions[(groupSizes.First() + 1)..], groupSizes[1..]);
+            totalArrangements += CalculateArrangements(conditions[(groupSizes.First() + 1)..], groupSizes[1..], memo);
         }
 
         if (conditions[0] is not '#')
-            totalArrangements += CalculateArrangements(conditions[1..], groupSizes);
+            totalArrangements += CalculateArrangements(conditions[1..], groupSizes, memo);
+
+        memo.TryAdd(GetMemoKey(conditions, groupSizes), totalArrangements);
 
         return totalArrangements;
+    }
+
+    string GetMemoKey(string conditions, List<int> groupSizes)
+    {
+        return $"{conditions}{string.Join(',', groupSizes)}";
     }
 
     struct Record(string conditions, List<int> groupSizes)
