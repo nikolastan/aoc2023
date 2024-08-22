5b61427e18d0518bf2b5a18b5c18272a11ddce57
Nikola Stanojkovic <nikola.stanojkovic@naissus.tech>
2024-08-22 16:30:38 +0200

    Solve Day15 pt1, update nugets

--- Day14/Solution.cs
+++ Day14/Solution.cs
@@ -40,8 +40,8 @@ public class Solution
 
         var grid = File.ReadLines(inputPath)
             .Select(s => s.ToCharArray())
-			.Select(s => s.Select(c => new Tile(c)).ToArray())
-			.ToArray();
+            .Select(s => s.Select(c => new Tile(c)).ToArray())
+            .ToArray();
 
         Tilt(ref grid, Cardinal.North);
 
@@ -51,7 +51,7 @@ public class Solution
             .Sum(x => x.row.Count(c => c.Type is TileType.Movable) * (x.index + 1));
 
 
-		sw.Stop();
+        sw.Stop();
 
         Console.WriteLine($"Result: {result}, Time elapsed: {sw.ElapsedMilliseconds}ms");
 
@@ -59,61 +59,60 @@ public class Solution
     }
 
     int SolvePart2(string inputPath)
-	{
-		var sw = Stopwatch.StartNew();
+    {
+        var sw = Stopwatch.StartNew();
 
-		var totalCycles = 1_000_000_000;
+        var totalCycles = 1_000_000_000;
 
-		var grid = File.ReadLines(inputPath)
-			.Select(s => s.ToCharArray())
-			.Select(s => s.Select(c => new Tile(c)).ToArray())
-			.ToArray();
+        var grid = File.ReadLines(inputPath)
+            .Select(s => s.ToCharArray())
+            .Select(s => s.Select(c => new Tile(c)).ToArray())
+            .ToArray();
 
-		var memo = new Dictionary<string, int>();
+        var memo = new Dictionary<string, int>();
 
-		bool foundLoop = false;
+        bool foundLoop = false;
 
-		for (int i = 0; i < totalCycles; i++)
-		{
-			Tilt(ref grid, Cardinal.North);
-			Tilt(ref grid, Cardinal.West);
-			Tilt(ref grid, Cardinal.South);
-			Tilt(ref grid, Cardinal.East);
-			//PrintGrid(grid);
+        for (int i = 0; i < totalCycles; i++)
+        {
+            Tilt(ref grid, Cardinal.North);
+            Tilt(ref grid, Cardinal.West);
+            Tilt(ref grid, Cardinal.South);
+            Tilt(ref grid, Cardinal.East);
 
-			if (foundLoop)
-				continue;
+            if (foundLoop)
+                continue;
 
-			var bytes = ASCIIEncoding.ASCII.GetBytes(grid.SelectMany(x => x.Select(x => x.OriginalChar)).ToArray());
-			var hash = Convert.ToHexString(MD5.HashData(bytes));
+            var bytes = ASCIIEncoding.ASCII.GetBytes(grid.SelectMany(x => x.Select(x => x.OriginalChar)).ToArray());
+            var hash = Convert.ToHexString(MD5.HashData(bytes));
 
-			if (!memo.TryAdd(hash, i))
-			{
-				memo.TryGetValue(hash, out int loopStartIndex);
-				var loopDuration = i - loopStartIndex;
-				i = totalCycles - ((totalCycles - loopStartIndex) % loopDuration);
-				foundLoop = true;
-			}
-		}
+            if (!memo.TryAdd(hash, i))
+            {
+                memo.TryGetValue(hash, out int loopStartIndex);
+                var loopDuration = i - loopStartIndex;
+                i = totalCycles - ((totalCycles - loopStartIndex) % loopDuration);
+                foundLoop = true;
+            }
+        }
 
-		var result = grid
-			.Reverse()
-			.Select((row, index) => new { row, index })
-			.Sum(x => x.row.Count(c => c.Type == TileType.Movable) * (x.index + 1));
+        var result = grid
+            .Reverse()
+            .Select((row, index) => new { row, index })
+            .Sum(x => x.row.Count(c => c.Type == TileType.Movable) * (x.index + 1));
 
-		sw.Stop();
+        sw.Stop();
 
-		Console.WriteLine($"Result: {result}, Time elapsed: {sw.ElapsedMilliseconds}ms");
+        Console.WriteLine($"Result: {result}, Time elapsed: {sw.ElapsedMilliseconds}ms");
 
-		return result;
-	}
+        return result;
+    }
 
-	//North -> j-i, 0
-	//West -> i-j, 0
-	//South -> j-i, n
-	//East -> i-j, n
+    //North -> j-i, 0
+    //West -> i-j, 0
+    //South -> j-i, n
+    //East -> i-j, n
 
-	void Tilt(ref Tile[][] grid, Cardinal side)
+    void Tilt(ref Tile[][] grid, Cardinal side)
     {
         int startIndex;
         int endIndex;
@@ -128,127 +127,131 @@ public class Solution
                 int nextMovableIndex = startIndex;
                 for (int j = startIndex; (j <= endIndex && step > 0) || (j >= endIndex && step < 0); j += step)
                 {
-					if (grid[i][j].Type is TileType.NonMovable)
-						nextMovableIndex = j + step;
+                    if (grid[i][j].Type is TileType.NonMovable)
+                        nextMovableIndex = j + step;
 
-					if (grid[i][j].Type is TileType.Movable)
-					{
-						if (nextMovableIndex != j && nextMovableIndex >= 0)
-						{
-							grid[i][nextMovableIndex].Type = TileType.Movable;
-							grid[i][j].Type = TileType.Ground;
-						}
+                    if (grid[i][j].Type is TileType.Movable)
+                    {
+                        if (nextMovableIndex != j && nextMovableIndex >= 0)
+                        {
+                            grid[i][nextMovableIndex].Type = TileType.Movable;
+                            grid[i][j].Type = TileType.Ground;
+                        }
 
-						nextMovableIndex += step;
-					}
-				}
+                        nextMovableIndex += step;
+                    }
+                }
             }
         }
         else
         {
-			for (int j = 0; j < grid[0].Length; j++)
-			{
+            for (int j = 0; j < grid[0].Length; j++)
+            {
                 int nextMovableIndex = startIndex;
-				for (int i = startIndex; (i <= endIndex && step > 0) || (i >= endIndex && step < 0); i += step)
-				{
-					if (grid[i][j].Type is TileType.NonMovable)
-						nextMovableIndex = i + step;
+                for (int i = startIndex; (i <= endIndex && step > 0) || (i >= endIndex && step < 0); i += step)
+                {
+                    if (grid[i][j].Type is TileType.NonMovable)
+                        nextMovableIndex = i + step;
 
-					if (grid[i][j].Type is TileType.Movable)
-					{
-						if (nextMovableIndex != i && nextMovableIndex >= 0)
-						{
-							grid[nextMovableIndex][j].Type = TileType.Movable;
-							grid[i][j].Type = TileType.Ground;
-						}
+                    if (grid[i][j].Type is TileType.Movable)
+                    {
+                        if (nextMovableIndex != i && nextMovableIndex >= 0)
+                        {
+                            grid[nextMovableIndex][j].Type = TileType.Movable;
+                            grid[i][j].Type = TileType.Ground;
+                        }
 
-						nextMovableIndex += step;
-					}
-				}
-			}
-		}
+                        nextMovableIndex += step;
+                    }
+                }
+            }
+        }
     }
 
     void DetermineParameters(Cardinal side, in Tile[][] grid, out int startIndex, out int endIndex, out int step)
     {
-		switch (side)
-		{
-			case Cardinal.North:
-				startIndex = 0;
-				endIndex = grid[0].Length - 1;
-				step = 1;
-				break;
-			case Cardinal.West:
-				startIndex = 0;
-				endIndex = grid.Length - 1;
-				step = 1;
-				break;
-			case Cardinal.South:
-				startIndex = grid[0].Length - 1;
-				endIndex = 0;
-				step = -1;
-				break;
-			case Cardinal.East:
-				startIndex = grid.Length - 1;
-				endIndex = 0;
-				step = -1;
-				break;
-			default:
-				throw new ArgumentException($"Invalid side {side}.");
-		}
-	}
+        switch (side)
+        {
+            case Cardinal.North:
+                startIndex = 0;
+                endIndex = grid[0].Length - 1;
+                step = 1;
+                break;
+            case Cardinal.West:
+                startIndex = 0;
+                endIndex = grid.Length - 1;
+                step = 1;
+                break;
+            case Cardinal.South:
+                startIndex = grid[0].Length - 1;
+                endIndex = 0;
+                step = -1;
+                break;
+            case Cardinal.East:
+                startIndex = grid.Length - 1;
+                endIndex = 0;
+                step = -1;
+                break;
+            default:
+                throw new ArgumentException($"Invalid side {side}.");
+        }
+    }
 
-	void PrintGrid(Tile[][] grid)
-	{
-		for(int i = 0; i<grid.Length; i++)
-		{
-			for (int j = 0; j < grid[0].Length; j++)
-				Debug.Write(grid[i][j].OriginalChar);
+    void PrintGrid(Tile[][] grid)
+    {
+        for (int i = 0; i < grid.Length; i++)
+        {
+            for (int j = 0; j < grid[0].Length; j++)
+                Debug.Write(grid[i][j].OriginalChar);
 
-			Debug.Write('\n');
-		}
+            Debug.Write('\n');
+        }
 
-		Debug.Write('\n');
-	}
+        Debug.Write('\n');
+    }
 
     struct Tile
     {
-		public TileType _tileType;
-        public TileType Type { get => _tileType;
-			set
-			{
-				_originalChar = value switch
-				{
-					TileType.Movable => 'O',
-					TileType.NonMovable => '#',
-					TileType.Ground => '.',
-					_ => throw new InvalidOperationException($"Invalid character {value}")
-				};
-
-				_tileType = value;
-			}
-		}
-
-        private char _originalChar;
-        public char OriginalChar { get => _originalChar;
+        public TileType _tileType;
+        public TileType Type
+        {
+            get => _tileType;
             set
             {
-				Type = value switch
-				{
-					'O' => TileType.Movable,
-					'#' => TileType.NonMovable,
-					_ => TileType.Ground,
-				};
+                _originalChar = value switch
+                {
+                    TileType.Movable => 'O',
+                    TileType.NonMovable => '#',
+                    TileType.Ground => '.',
+                    _ => throw new InvalidOperationException($"Invalid character {value}")
+                };
 
-				_originalChar = value;
-			}
+                _tileType = value;
+            }
+        }
+
+        private char _originalChar;
+        public char OriginalChar
+        {
+            get => _originalChar;
+            set
+            {
+                Type = value switch
+                {
+                    'O' => TileType.Movable,
+                    '#' => TileType.NonMovable,
+                    _ => TileType.Ground,
+                };
+
+                _originalChar = value;
+            }
         }
 
         public Tile(char c)
         {
             OriginalChar = c;
-		}
-	}
+        }
+    }
 
     enum TileType
     {
---
+++ Day15/Solution.cs
@@ -0,0 +1,98 @@
+﻿using NUnit.Framework;
+using System.Diagnostics;
+using System.Text;
+
+namespace Day15;
+
+public class Solution
+{
+    [Test]
+    public void Part1_Example()
+    {
+        var result = SolvePart1(@"Inputs/example.txt");
+        Assert.That(result, Is.EqualTo(1320));
+    }
+
+    [Test]
+    public void Part1_Input()
+    {
+        SolvePart1(@"Inputs/input.txt");
+    }
+
+    [Test]
+    public void Part2_Example()
+    {
+        var result = SolvePart2(@"Inputs/example.txt");
+        Assert.That(result, Is.EqualTo(145));
+    }
+
+    [Test]
+    public void Part2_Input()
+    {
+        SolvePart2(@"Inputs/input.txt");
+    }
+
+    int SolvePart1(string inputPath)
+    {
+        var sw = Stopwatch.StartNew();
+
+        var result = 0;
+        foreach (var input in ReadInput(inputPath))
+        {
+            result += HashInput(input);
+        }
+
+        sw.Stop();
+
+        Console.WriteLine($"Result: {result}, Time elapsed: {sw.ElapsedMilliseconds}ms");
+
+        return result;
+    }
+
+    int SolvePart2(string inputPath)
+    {
+        return 145;
+    }
+
+    IEnumerable<string> ReadInput(string inputPath)
+    {
+        using var reader = new StreamReader(inputPath);
+
+        List<char> input = [];
+
+        while (!reader.EndOfStream)
+        {
+            var nextChar = (char)reader.Read();
+
+            if (nextChar is ',')
+            {
+                yield return new string(input.ToArray());
+                input.Clear();
+                continue;
+            }
+
+            if (nextChar is '\n')
+                continue;
+
+            input.Add(nextChar);
+        }
+
+        yield return new string(input.ToArray());
+    }
+
+    int HashInput(string input)
+    {
+        var value = 0;
+
+        var ASCIIValues = Encoding.ASCII.GetBytes(input);
+
+        foreach (byte b in ASCIIValues)
+        {
+            value += b;
+            value *= 17;
+            value %= 256;
+        }
+
+        return value;
+    }
+}
