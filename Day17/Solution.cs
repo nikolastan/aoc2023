f21c67d6e0c852b8fb62c4f513c4207b01a66fc8
Nikola Stanojkovic <nikola.stanojkovic@naissus.tech>
2024-09-05 16:39:50 +0200

    Update nugets, Day17 pt1 WIP

---
+++ Day17/Solution.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System.Diagnostics;
+
+namespace Day17;
+
+public class Solution
+{
+    [Test]
+    public void Part1_Example()
+    {
+        var result = SolvePart1(@"Inputs/example.txt");
+        Assert.That(result, Is.EqualTo(102));
+    }
+
+    [Test]
+    public void Part1_Input()
+    {
+        SolvePart1(@"Inputs/input.txt");
+    }
+
+    int SolvePart1(string inputPath)
+    {
+        var sw = Stopwatch.StartNew();
+
+        var grid = File.ReadLines(inputPath)
+            .Select(line => line.Select(x => (int)x).ToArray())
+            .ToArray();
+
+
+
+        var result = 102;
+
+        sw.Stop();
+
+        Console.WriteLine($"Result: {result}, Time elapsed: {sw.ElapsedMilliseconds}ms");
+
+        return result;
+    }
+
+    int CalculateMinRoad(int[][] map, int i, int j, Direction currentDirection, int directionCount)
+    {
+
+    }
+
+    enum Direction
+    {
+        Down, Left, Right
+    }
+}
